b4070c85dd646be1cda0ecd96af60d7d908f5db7
Thinesh Shrinivas <shrinivas.thinesh@smartbear.com>
2020-03-30 18:33:58 +0530

    initial app config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path= require("path");
+const webpackMerge = require('webpack-merge');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports=()=>{
+    return webpackMerge({
+        entry:'./src/index.js',
+        output:{
+            path:path.resolve(__dirname,'dist'),
+            filename:'bundel.js',
+            library:"CommonComp",
+            libraryTarget:'umd'
+        },
+        module:{
+            rules:[
+                {test:/\.m?js$/,loader:'babel-loader'},
+                {test:/\.(css|less|scss)$/,loader:[miniCssExtractPlugin.loader,'css-loader']},
+                {test:/\.(svg|png|jpe?g)$/,loader:'url-loader'}
+            ]
+        },
+      devServer:{
+          historyApiFallback:true,
+          port:9095
+      },
+      plugins:[new htmlWebpackPlugin({template:'./public/index.html'}), new miniCssExtractPlugin()]  
+    })
+}
